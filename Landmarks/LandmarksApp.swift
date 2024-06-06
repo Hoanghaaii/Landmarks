9055107403a98b3ad4afc735e91780ad98d74ce2
Hoanghaaii <haivu04112003@gmail.com>
2024-06-06 10:23:50 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by HoangHaaii on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
